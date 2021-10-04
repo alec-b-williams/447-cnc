a5e26e00fd14d80d6b3d1badf6eb6cb0e05d7625
Alec Williams <alec.b.williams@wsu.edu>
2021-10-04 15:12:33 -0700

    Added wall placing/removal
    -Implemented extremely basic shop system
    -Added system to allow for the removal of crops and walls (returning
    them to empty soil tiles)

--- src/cnc/CropGame.java
+++ src/cnc/CropGame.java
@@ -7,37 +7,12 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
-import java.sql.Array;
 import java.util.ArrayList;
 
 /**
- * A Simple Game of Bounce.
+ * Crops & Crossbows
  *
- * The game has three states: StartUp, Playing, and GameOver, the game
- * progresses through these states based on the user's input and the events that
- * occur. Each state is modestly different in terms of what is displayed and
- * what input is accepted.
- *
- * In the playing state, our game displays a moving rectangular "ball" that
- * bounces off the sides of the game container. The ball can be controlled by
- * input from the user.
- *
- * When the ball bounces, it appears broken for a short time afterwards and an
- * explosion animation is played at the impact site to add a bit of eye-candy
- * additionally, we play a short explosion sound effect when the game is
- * actively being played.
- *
- * Our game also tracks the number of bounces and syncs the game update loop
- * with the monitor's refresh rate.
- *
- * Graphics resources courtesy of qubodup:
- * http://opengameart.org/content/bomb-explosion-animation
- *
- * Sound resources courtesy of DJ Chronos:
- * http://www.freesound.org/people/DJ%20Chronos/sounds/123236/
- *
- *
- * @author wallaces
+ * @author alec.b.williams
  *
  */
 public class CropGame extends StateBasedGame {
@@ -61,9 +36,12 @@ public class CropGame extends StateBasedGame {
 	public final int ScreenWidth;
 	public final int ScreenHeight;
 
+	public int shopIndex;
 	public int level;
 	public ArrayList<Tile> tiles = new ArrayList<Tile>();
 	public ArrayList<Crop> crops = new ArrayList<Crop>();
+
+
 
 	/**
 	 * Create the BounceGame frame, saving the width and height for later use.
@@ -101,6 +79,7 @@ public class CropGame extends StateBasedGame {
 		ResourceManager.loadImage(SUNFLOWER_IMG_RSC);
 
 		level = 1;
+		shopIndex = 0;
 
 		tiles = Levels.generateField(Levels.levelList[level-1]);
 	}
